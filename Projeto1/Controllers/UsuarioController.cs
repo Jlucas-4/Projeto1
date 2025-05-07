2c1a0828b1167c0315ff449587e1c1da5294a8f7
JoaoL <jlucaspemaciel>
2025-05-07 08:45:00 -0300

    Criando uma requisicao post

--- Projeto1/Controllers/UsuarioController.cs
+++ Projeto1/Controllers/UsuarioController.cs
@@ -1,10 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Projeto1.Repositorio;
 
 namespace Projeto1.Controllers
 {
     public class UsuarioController : Controller
     {
-        public IActionResult Index()
+        /* DECLAANDO UMA VARIAVEL PRIVADA SOMENTE PARA LEITURA DO TIPO 
+         * UsuarioRepositorio chamada _usuarioRepositorio */
+        private readonly UsuarioRepositorio _usuarioRepositorio;
+        // DEFININDO O CONSTRUTOR DA CLASSE UsuarioController QUE VAI RECEBER UMA INSTANCIA DE UM UsuarioRepositorio
+        public UsuarioController (UsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Contato()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string email, string senha)
         {
             return View();
         }
